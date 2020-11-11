a2c42a16a8c94cc59e84bcc866ac800c468aa1eb
shayanja <FA28080D091ACED0>
2020-11-10 20:21:07 -0800

    Update tweetFactory address

--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -2,7 +2,7 @@
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
   ceaErc20: "0xc1C0472c0C80bCcDC7F5D01A376Bd97a734B8815",
-  tweetFactory: "0x61ef061EC25f935b8c35F2ACb1Ff4FC16de067CC"
+  tweetFactory: "0x31763F3CA7fe5CE7968651e21Af69B8b3dD64c7e"
 };
 
 export default addresses;
