8419f1b86ad9703b9f530596370c0117b2f87e06
shayanja <FA28080D091ACED0>
2020-11-10 18:43:32 -0800

    Update address

--- packages/contracts/src/addresses.js
+++ packages/contracts/src/addresses.js
@@ -2,7 +2,7 @@
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
   ceaErc20: "0xc1C0472c0C80bCcDC7F5D01A376Bd97a734B8815",
-  tweetFactory: "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+  tweetFactory: "0x61ef061EC25f935b8c35F2ACb1Ff4FC16de067CC"
 };
 
 export default addresses;
