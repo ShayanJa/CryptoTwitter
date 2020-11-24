4bae7ae7520a0fb56082ef3645dfb8a42050c9a4
shayanja <FA28080D091ACED0>
2020-11-23 20:21:03 -0800

    Update homepage when not connected

--- packages/react-app/pages/index.js
+++ packages/react-app/pages/index.js
@@ -5,10 +5,11 @@ import styles from './index.module.css'
 import Layout from '../components/layout'
 import Tweet from '../components/tweet'
 import Loading from '../components/loading'
-import { useTweets } from '../store/web3/hooks'
+import { useTweets, useWalletConnect } from '../store/web3/hooks'
 
 function HomePage() {
   const [tweets, _, getTweets] = useTweets()
+  const [address, connectWallet] = useWalletConnect()
 
   useEffect(() => {
     getTweets(15)
@@ -16,14 +17,18 @@ function HomePage() {
 
   return (
     <Layout>
-      {tweets && tweets.length > 0 ? (
-        tweets.map((tweet) => {
-          return <Tweet {...tweet}></Tweet>
-        })
+      {address ? (
+        tweets && tweets.length > 0 ? (
+          tweets.map((tweet) => {
+            return <Tweet {...tweet}></Tweet>
+          })
+        ) : (
+          <div className={styles.loading}>
+            <Loading />
+          </div>
+        )
       ) : (
-        <div className={styles.loading}>
-          <Loading />
-        </div>
+        <p>Connect wallet on Kovan</p>
       )}
     </Layout>
   )
